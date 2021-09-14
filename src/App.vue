adeab20bb62df2517ca88605b56a3a83c449cd97
Ayeni Olakunle <ayeniolakunle98@gmail.com>
2021-09-14 20:47:04 +0100

    Todo App Using Vue

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <Todos/>
+</template>
+
+<script>
+import Todos from './components/Todos.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Todos
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
